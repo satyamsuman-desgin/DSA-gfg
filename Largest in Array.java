0116c5a4a80a2f7ffafc1daa2e938f1581d373b1
Satyam kr Suman <jhalina3255@gmail.com>
2026-09-07 22:25:08 +0530

    Add method to find largest element in array
    Implement method to find the largest number in an array.

---
+++ Largest in Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int largest(int[] arr) {
+        // code here
+         int largest = Integer.MIN_VALUE;
+        for(int i =0; i<arr.length;i++){
+            if(arr[i]>largest){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+}
