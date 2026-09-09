e794c5e61200f4585a7b1b1188d83b69651b2877
Satyam kr Suman <jhalina3255@gmail.com>
2026-09-09 23:28:58 +0530

    Implement sum method in Solution class

---
+++ sum of arra.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int sum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
