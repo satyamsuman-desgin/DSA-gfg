9c791c319913b5a5201c66cffd5e88248bed1660
Satyam kr Suman <jhalina3255@gmail.com>
2026-09-11 23:15:29 +0530

    Add removeDuplicates method to Solution class
    Implement method to remove duplicates from a sorted array.

---
+++ Remove Duplicates Sorted Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    ArrayList<Integer> removeDuplicates(int[] arr) {
+        // code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        ans.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                ans.add(arr[i]);
+            }
+        }
+        return ans;
+    }
+}
